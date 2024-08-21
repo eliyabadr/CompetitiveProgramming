1c9e4f557d11e3acb7d68fe63183d092ed7e7b94
eliya <eliyabadr2@gmail.com>
2024-08-21 13:57:32 +0300

    update

--- CodeForces/2004c.cpp
+++ CodeForces/2004c.cpp
@@ -1,30 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve(){
+	int n,k; cin>>n>>k;
+	vector<int> v(n);
+	for(auto &x : v) cin>>x;
+	sort(v.begin(), v.end(), greater<int>());
+	int sum = 0;	
+	int p = 1;
+	for(int i = 0 ; i < n ; i++){
+		if(i%2 != 0 && k > 0){
+			int x = v[i];
+			v[i] = min(v[i-1] , v[i]+k);
+			k -= abs(v[i]-x);	
+		}
+		sum += p*v[i];
+		p = (-1)*p;
+	}
+	cout<<sum<<endl;
+}
+
 int main(){
 	int t; cin>>t;
 	while(t--){
-		int n,k; cin>>n>>k;
-		vector<int> v(n);
-		for(auto &x : v){cin>>x;}
-		sort(v.begin() , v.end());
-		if(n == 1){
-			cout<<v[0]<<endl; continue;
-		}
-		for(int i = n-1; i >= (n%2) + 1; i--){
-			int j = i-1;
-			v[j] += min(k, abs(v[i]-v[j]));
-			k -= min(k, abs(v[i]-v[j]));
-			if(k == 0){break;}
-		}
-
-		int sum = 0;
-		int p = 1;
-		for(int i = 0 ; i < n ; i++){
-			sum += v[n-i-1]*p;
-			p = p * (-1);
-		}
-		cout<<sum<<endl;
+		solve();
 	}
 	return 0;
 }
